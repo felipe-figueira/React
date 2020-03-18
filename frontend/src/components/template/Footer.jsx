1da5d9e81fc20014e5facfc22b3f71bc5515da14
felipe-figueira <felipe.figueira13@gmail.com>
2020-03-18 17:45:58 -0300

    Projeto Concluido

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+    <footer className="footer">
+        <span>
+            Desenvolvido por 
+            Felipe Figueira.
+        </span>
+    </footer>
---
+++ frontend/src/components/template/Logo.jsx
@@ -0,0 +1,11 @@
+import './Logo.css'
+import { Link } from 'react-router-dom'
+
+import React from 'react';
+
+export default props =>
+    <aside className="logo">
+        <Link to="/" className="logo">
+            
+        </Link>
+    </aside>
